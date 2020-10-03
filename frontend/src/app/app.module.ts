2f17d167886bca94c94afaf3464e8f744f586280
Victor Pereira <victor.cordeiro2@gmail.com>
2020-10-03 15:55:31 -0300

    Inserindo Grid e Responsividade

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 import { LiveListComponent } from './views/home/live-list/live-list.component';
 
 @NgModule({
@@ -24,7 +25,8 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
